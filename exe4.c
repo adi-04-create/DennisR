52d0c319a647ad331c5aab66521eb19cb145add1
aditya <aditya.nathwani@einfochips.com>
2025-01-28 16:19:38 +0530

    Example for yocto recipe

---
+++ exe4.c
@@ -0,0 +1,19 @@
+/* this program is to print the corresponding Celsius to Fahrenheit table.
+author: Aditya Nathwani
+creted:26 dec,2024
+modified:26 dec,2024
+*/
+
+
+
+/* reqired header file */
+#include<stdio.h>
+
+/* start of main function */
+int main(){
+
+	printf("Celsius to Fahrenheit table\n");
+	printf("\nCelsius Fahrenheit\n");// to print header of Fahrenheit-Celsius Table
+	celsiustofahrenheit();
+
+}
